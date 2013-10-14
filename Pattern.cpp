615e0db6bec4d8ee38a18dd14658579fad1408c1
Eduardo Jesus <eduardo.fernando.jesus@gmail.com>
2013-10-14 21:29:55 +0100

    corrected a bug that would not show the camera image if there was no pattern on it.

--- Pattern.cpp
+++ Pattern.cpp
@@ -8,7 +8,7 @@ Pattern::Pattern() {
 Pattern::Pattern(std::string name, double width, double center[]) {
     name_ = name;
     width_ = width;
-    std::copy(center, center_, sizeof(center));
+    //std::copy(center, center_, sizeof(center));
 }
 
 Pattern::~Pattern() {}
